1564391306263fc3cdccff7bd373be3a634188e8
1uciferx <portforwarding03@gmail.com>
2023-02-07 17:38:49 +0530

    tabs

--- client/navigation/tabs.js
+++ client/navigation/tabs.js
@@ -1,16 +1,56 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from '../screens/HomeScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={""} />
-    </Tab.Navigator>
+    
+        <Tab.Navigator
+          initialRouteName="Home"
+          shifting={true}
+          activeColor="white"
+          inactiveColor="grey"
+          barStyle={{ backgroundColor: 'black' }}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={"black"} size={26} />
+            ),
+            tabBarLabel: 'Home',
+          }} />
+          <Tab.Screen name="Analysis" component={""} options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="atom-variant" color={color} size={26} />
+            ),
+            tabBarLabel: 'Analysis',
+          }} />
+          <Tab.Screen name="Trading" component={""} options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="arrow-collapse" color={color} size={26} />
+            ),
+            tabBarLabel: 'Trading',
+          }} />
+          <Tab.Screen name="Wishlist" component={""} options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="cards-heart" color={color} size={26} />
+            ),
+            tabBarLabel: 'Wishlist',
+          }} />
+          <Tab.Screen name="Profile" component={""} options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account-reactivate" color={color} size={26} />
+            ),
+            tabBarLabel: 'Profile',
+          }} />
+        </Tab.Navigator>
+  
   );
 }
+
 
 
 export default MyTabs;
--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -5,13 +5,16 @@ import {
   Image,
   TextInput,
   ScrollView,
+  StyleSheet
 } from "react-native";
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Feather, Ionicons } from "@expo/vector-icons";
 import Categories from "../components/Categories";
 import FeaturedRow from "../components/FeaturedRow";
-import sanityClient from "../sanity";
+import { LinearGradient } from 'expo-linear-gradient';
+
+
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -25,71 +28,30 @@ const HomeScreen = () => {
   }, []);
 
   //when component loads
-  useEffect(() => {
-    sanityClient
-      .fetch(
-        `
-    *[_type == "featured"]{
-      ...,
-      restaurants[]->{
-        ...,
-        dishes[]->
-      }
-    }
-    `
-      )
-      .then((data) => {
-        setFeaturedCategories(data);
-      });
-  }, []);
 
   return (
-    <SafeAreaView className="bg-white pt-5 ">
-      <View className="flex-row pb-3 items-center mx-3 space-x-2 mt-[30px]">
-        <Image
-          source={{
-            uri: "https://upload.wikimedia.org/wikipedia/commons/7/75/Zomato_logo.png",
-          }}
-          className="h-10 w-10 bg-gray-300 p-4 rounded-full"
-        />
-        <View className="flex-1">
-          {/* <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text> */}
-          <Text className="font-bold text-xl">
-            Current Location
-            <Feather name="chevron-down" size={20} color="#E33342" />
-          </Text>
-        </View>
-        <Ionicons name="person-outline" size={35} color="#E33342" />
-      </View>
-
-      <View className="flex-row items-center space-x-2 mx-4 pb-2">
-        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
-          <Ionicons name="search-outline" size={20} color="gray" />
-          <TextInput placeholder="Restaurants and cuisines" />
-        </View>
-        <Feather name="sliders" size={20} color="#E33342" />
-      </View>
-
-      <ScrollView
-        className="bg-gray-100"
-        contentContainerStyle={{
-          paddingBottom: 100,
-        }}
-        showsVerticalScrollIndicator={false}
-      >
-        <Categories />
-
-        {featuredCategories.map((category) => (
-          <FeaturedRow
-            key={category._id}
-            id={category._id}
-            title={category.name}
-            description={category.short_description}
-          />
-        ))}
-      </ScrollView>
-    </SafeAreaView>
+    // <SafeAreaView className="bg-white pt-5 ">
+    <LinearGradient
+      colors={['#684A3F', '#0B0100']}
+      start={[1, 0]}
+      end={[1, 1]}
+      style={styles.container}
+    >
+    </LinearGradient>
   );
+
+
+
+  // </SafeAreaView>
+
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+});
+
 
 export default HomeScreen;
